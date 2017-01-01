5e0ffb9755c0b14554f7df371c53f1140a052a15
elevenBeans <elevenbeansf2e@gmail.com>
2017-01-01 23:01:22 +0800

    unifiy start file -- start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 # compile resource file
+echo '\033[0;32;1m######################\033[0m'
 echo '\033[0;32;1mcompiling resource ...\033[0m'
+echo '\033[0;32;1m######################\033[0m'
+
 npm run build
 
 # watch resource file
-
+echo '\033[0;32;1m#####################################################\033[0m'
 echo '\033[0;32;1mfinished compiling, now start server and watching ...\033[0m'
+echo '\033[0;32;1m#####################################################\033[0m'
+
+echo '\033[0;33;1msee it in http://localhost:8000\033[0m'
 
 npm run watch &
 
