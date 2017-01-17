af67990ab1b8af34438c3a70804bd47c721d65c5
elevenBeans <elevenbeansf2e@gmail.com>
2017-01-17 15:20:08 +0800

    add eslintrc

--- public/javascripts/home/index.jsx
+++ public/javascripts/home/index.jsx
@@ -1,17 +1,17 @@
-'use strict'
+'use strict';
 
 import '../../stylesheets/global.less';
 import '../../stylesheets/flight.less';
 
-import $ from 'zepto';
+// import $ from 'zepto';
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import createHistory from 'history/createBrowserHistory';
+// import createHistory from 'history/createBrowserHistory';
 
-import {RouteTransition} from 'react-router-transition';
-import {presets} from 'react-router-transition';
+import { RouteTransition } from 'react-router-transition';
+import { presets } from 'react-router-transition';
 
 import Home from 'home';
 import Header from 'header';
@@ -20,12 +20,12 @@ import FlightList from 'flightlist';
 import Train from 'train';
 import Hotel from 'hotel';
 
-if(module.hot && process.env.NODE_ENV === 'dev-HMR') module.hot.accept();
+if (module.hot && process.env.NODE_ENV === 'dev-HMR') module.hot.accept();
 
-console.log('process.env.NODE_ENV in Front-end:',process.env.NODE_ENV);
+console.log('process.env.NODE_ENV in Front-end:', process.env.NODE_ENV);
 
-const history = createHistory();
-const location = history.location;
+// const history = createHistory();
+// const location = history.location;
 
 var styles = presets.slideLeft;
 
@@ -35,15 +35,13 @@ var styles = presets.slideLeft;
 // })
 
 // Use push, replace, and go to navigate around.
-//history.push('/home', { some: 'state' })
+// history.push('/home', { some: 'state' })
 
 // To stop listening, call the function returned from listen().
-//unlisten()
+// unlisten()
 
-var App = function ({ children, location }) { //children = React.children
-  
-  styles = location.action === 'POP'? presets.slideRight: presets.slideLeft;
-
+var App = function({ children, location }) {
+  styles = location.action === 'POP' ? presets.slideRight : presets.slideLeft;
   return (
   <div>
     <Header />
@@ -57,8 +55,8 @@ var App = function ({ children, location }) { //children = React.children
     </RouteTransition>
 
   </div>
-  )
-}
+  );
+};
 
 render((<Router key={Math.random()} history={browserHistory} >
           <Route path="/" component={App}>
@@ -78,4 +76,4 @@ render((<Router key={Math.random()} history={browserHistory} >
           </Route>
         </Router>
   ), document.getElementById('app')
-)
+);
